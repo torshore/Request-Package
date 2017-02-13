67a9ed0065c3a1b0a1069fbf123bfd0af663b489
Tory Shoreman <toryshoreman@gmail.com>
2017-02-13 21:36:46 +0000

    Adding Solution to Exercise 2

---
+++ ex2.js
@@ -0,0 +1,15 @@
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+        .on('error', function (err) {                                   // Note 2
+           throw err;
+         })
+         .on('response', function (response) {                           // Note 3
+           console.log('Response Status Code: ', response.statusCode);
+           console.log('Response Message: ', response.statusMessage);
+           console.log('Content Type: ', response.headers['content-type']);
+         })
+         .pipe(fs.createWriteStream('./future.jpg'));
+
+console.log('Downloading image ...');
